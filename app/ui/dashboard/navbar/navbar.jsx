90a736bd89656fab7c1b47ddabe9e6fc3b772928
lipocodes <lipocodes@yahoo.com>
2023-12-07 13:45:12 +0200

    navbar_OK

--- app/ui/dashboard/navbar/navbar.jsx
+++ app/ui/dashboard/navbar/navbar.jsx
@@ -1,8 +1,35 @@
+"use client"
 import React from 'react'
+import styles from './navbar.module.css'
+import { usePathname } from 'next/navigation';
+import {
+  MdNotifications,
+  MdOutlineChat,
+  MdPublic,
+  MdSearch,
+} from "react-icons/md";
 
 function navbar() {
+
+  const pathname = usePathname(); 
+
   return (
-    <div>navbar</div>
+    <div className={styles.container}>
+      <div className={styles.title}>
+        {pathname.split('/').pop()}
+      </div>
+      <div className={styles.menu}>
+        <div className={styles.search}>
+          <MdSearch/>
+          <input type="text" placeholder='Search...' className={styles.input}/>
+        </div>
+        <div className={styles.icons}>
+          <MdOutlineChat size={20}/>
+          <MdNotifications size={20}/>
+          <MdPublic size={20}/>
+        </div>
+      </div>
+    </div>
   )
 }
 
